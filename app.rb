cdf7f39a516212cd72e10d5f85512acb71ba6d4f
Rohan Phillips <rohan@solutions-insurance.com>
2019-09-02 00:06:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get 'name' do
+    "My name is Rohan"
+  end
+
+  get 'hometown' do
+    "My hometown is Portland"
+  end
+
+  get 'favorite-song' do
+    "My favorite song is Sultans of Swing"
+  end
 end
